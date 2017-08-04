d07a2e4d80a3d7dec8a0a8a935bdc32f25cf4f70
Ralic Lo <raliclo@gmail.com>
2017-08-04 10:44:41 -0400

    More packages built using gcc7

---
+++ libxslt/1.1.29/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/linuxbrew/.linuxbrew/Cellar/libxslt/1.1.29/lib"
+XSLT_LIBS="-lxslt  -L/home/linuxbrew/.linuxbrew/Cellar/libxml2/2.9.4_3/lib -lxml2 -lz -lm -ldl -lm "
+XSLT_INCLUDEDIR="-I/home/linuxbrew/.linuxbrew/Cellar/libxslt/1.1.29/include"
+MODULE_VERSION="xslt-1.1.29"
